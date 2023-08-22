4b35994cfd522505d6da828a2ebf331bdef95fcb
czenhe <czenhe@gmail.com>
2023-08-22 18:57:53 +0900

    setting: github action

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <header class="globalHeader">
-      <h1>Lion Loves Me</h1>
+      <h1><span>사</span>자, <span>마</span>음 그리고 <span>나</span></h1>
     </header>
     <slot />
     <footer class="globalFooter">@ czenhe</footer>
